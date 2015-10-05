aa7390efca8fc85c2c639bda5cc4d2c49cd352ab
lancevalour <zhangyicheng1234@gmail.com>
2015-10-04 22:35:31 -0400

    Initial commit

---
+++ www/js/services.js
@@ -0,0 +1,7 @@
+angular.module('starter.services', ['ngResource'])
+
+angular.module('starter.services', ['ngResource'])
+
+.factory('Session', function ($resource) {
+    return $resource('http://localhost:5000/sessions/:sessionId');
+});
